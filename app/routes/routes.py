3604728481191fee726a6819c8c1bf57fb96eb94
KimberlyPocco <kimberly.poccop@gmail.com>
2023-03-02 18:17:23 -0500

    update structure

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,5 +1,11 @@
 from flask import Blueprint, render_template, redirect, request, session
+from flask_login import LoginManager,logout_user,login_required,current_user, login_user
 from app.db import db
 from datetime import datetime
+from werkzeug.urls import url_parse
 
 app_router = Blueprint("app_router", __name__)
+
+@app_router.route("/")
+def index():
+    return render_template("index.html")
